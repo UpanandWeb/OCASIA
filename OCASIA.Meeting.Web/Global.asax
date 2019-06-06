fdf78002d7c0f5b094027e80884924af64cc9ff1
UpanandWeb <40310199+UpanandWeb@users.noreply.github.com>
2019-06-06 11:47:51 +0530

    Add files via upload

---
+++ OCASIA.Meeting.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OCASIA.Meeting.Web.MvcApplication" Language="C#" %>
---
+++ OCASIA.Meeting.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OCASIA.Meeting.Web.Startup))]
+namespace OCASIA.Meeting.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
