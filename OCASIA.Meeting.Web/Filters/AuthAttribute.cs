082ba3a24b4902fbd61ce840d0b138e243b2c94a
UpanandWeb <40310199+UpanandWeb@users.noreply.github.com>
2019-06-06 12:18:47 +0530

    Add files via upload

---
+++ OCASIA.Meeting.Web/Filters/AuthAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace OCASIA.Meeting.Web.Filters
+{
+    public class AuthAttribute
+    {
+        public class SuperAdminAuthAttribute : FilterAttribute, IAuthenticationFilter
+        {
+            public void OnAuthentication(AuthenticationContext context)
+            {
+                if (context.HttpContext.User.Identity.IsAuthenticated)
+                {
+                    string CurrentRole = Helper.CurrentUserRole();
+                    if (CurrentRole != null)
+                    {
+                        // do proceed    
+                    }
+                    else
+                    {
+                        context.Result = new RedirectToRouteResult("Default",
+                      new System.Web.Routing.RouteValueDictionary{
+                        {"controller", "Home"},
+                        {"action", "PageNotFound"}
+                      });
+                    }
+                }
+                else
+                {
+                    context.Result = new HttpUnauthorizedResult(); // mark unauthorized
+                }
+            }
+
+            public void OnAuthenticationChallenge(AuthenticationChallengeContext context)
+            {
+                if (context.Result is HttpUnauthorizedResult)
+                {
+                    context.Result = new RedirectToRouteResult("Default",
+                       new System.Web.Routing.RouteValueDictionary{
+                        {"controller", "Account"},
+                        {"action", "Login"}
+                       });
+                }
+            }
+        }
+    }
+}
